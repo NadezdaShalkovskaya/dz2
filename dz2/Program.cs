8e5d2def3a89fd132374b150fc2ac8a1ed46ab2b
Надежда <@DESKTOP-EHSPKBR>
2024-11-21 00:11:53 +0200

    Добавьте файлы проекта.

---
+++ dz2/Program.cs
@@ -0,0 +1,9 @@
+﻿double start = 1;
+double  end = 29;
+double step  = 0.2;
+for (double x = start; x <= end; x += step)
+{
+    double y = Math.Log(x*x)- x + 4;
+    Console.WriteLine($"x:{x:F2},U:{y:F2}");
+}
+
